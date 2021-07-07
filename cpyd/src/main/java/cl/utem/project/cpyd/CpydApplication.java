9a8055c9c9409d55c9dd87848ee90a8b93ede044
esteeban <erojas@utem.cl>
2021-07-07 18:05:00 -0400

    Proyecto Inicial

---
+++ cpyd/src/main/java/cl/utem/project/cpyd/CpydApplication.java
@@ -0,0 +1,13 @@
+package cl.utem.project.cpyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpydApplication.class, args);
+	}
+
+}
